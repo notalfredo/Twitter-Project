dcdc3f6068e0518713628a21bd62f96a844a9ae3
Alfredo <icealfredo123@gmail.com>
2021-10-01 16:31:08 -0500

    Pushing all these files

--- Twitter/HomeTableViewController.swift
+++ Twitter/HomeTableViewController.swift
@@ -24,12 +24,14 @@ class HomeTableViewController: UITableViewController {
          
         myRefreshControl.addTarget(self, action: #selector(loadTweets), for: .valueChanged)
         
-        tableView.refreshControl = myRefreshControl
+        self.tableView.refreshControl = myRefreshControl
+        self.tableView.rowHeight = UITableView.automaticDimension
+        self.tableView.estimatedRowHeight = 150
 
         
         
     }
-
+    
     
     
     @objc func loadTweets(){
@@ -142,7 +144,10 @@ class HomeTableViewController: UITableViewController {
         if let imageData = data {
             cell.profileImageView.image = UIImage(data: imageData)
         }
-        
+        cell.setFavorite(tweetArray[indexPath.row]["favorited"] as! Bool)
+        cell.tweetId = tweetArray[indexPath.row]["id"] as! Int
+        cell.setRetweeted(tweetArray[indexPath.row]["retweeted"] as! Bool)
+
         
         return cell
     }
